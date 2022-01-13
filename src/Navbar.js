c1dcf033be6bf1a0c5bfe348c2275de11b87b154
finn-01 <nguyensam576@gmail.com>
2022-01-13 10:33:00 +0700

    set up

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+
+import { links, social } from "./data";
+import logo from "./logo.svg";
+
+const Navbar = () => {
+	return <div>Hello navbar</div>;
+};
+
+export default Navbar;
